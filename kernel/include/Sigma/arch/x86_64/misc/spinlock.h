fd60202fc9d6e5adb375aabe80093dff874ae604
Thomas Woertman <twoertman@gmail.com>
2019-08-20 20:25:57 +0200

    Add new spinlock locking mechanism

--- kernel/include/Sigma/arch/x86_64/misc/spinlock.h
+++ kernel/include/Sigma/arch/x86_64/misc/spinlock.h
@@ -4,13 +4,22 @@
 #include <Sigma/common.h>
 
 namespace x86_64::spinlock {
-    struct mutex {
-        mutex(): lock(0) {}
-        uint16_t lock;
-    };
+    struct mutex;
 
     C_LINKAGE void acquire(mutex* lock);
     C_LINKAGE void release(mutex* lock);
+
+    struct mutex {
+        mutex(): lock(0) {}
+        uint16_t lock;
+        void acquire(){
+            x86_64::spinlock::acquire(this);
+        }
+
+        void release(){
+            x86_64::spinlock::release(this);
+        }
+    };
 }
 
 #endif
