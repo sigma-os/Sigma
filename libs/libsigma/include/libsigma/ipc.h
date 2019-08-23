d47efc6ded8d02968a274726a5f9018bb5991ba1
Thomas Woertman <twoertman@gmail.com>
2019-08-23 20:01:40 +0200

    Add skeleton for standard IPC messages

--- libs/libsigma/include/libsigma/ipc.h
+++ libs/libsigma/include/libsigma/ipc.h
@@ -8,9 +8,21 @@ extern "C" {
 #elif defined(__STDC__)
 #include <stdint.h>
 #include <stddef.h>
+#include <stdbool.h>
 #else 
 #error "Compiling libsigma/ipc.h on unknown language"
 #endif
+
+
+typedef struct libsigma_message {
+    uint64_t command;
+    uint8_t checksum;
+    // TODO, anything more?
+    uint8_t command_data[];
+} libsigma_message_t;
+
+bool libsigma_ipc_check_message(libsigma_message_t* msg, size_t size);
+void libsigma_ipc_set_message_checksum(libsigma_message_t* msg, size_t size);
 
 // TODO: tid_t
 int libsigma_ipc_send(uint64_t dest, size_t buf_size, uint8_t* buffer);
--- libs/libsigma/source/ipc.c
+++ libs/libsigma/source/ipc.c
@@ -5,6 +5,23 @@
 extern "C" {
 #endif
 
+bool libsigma_ipc_check_message(libsigma_message_t* msg, size_t size){
+    uint8_t check = 0;
+    uint8_t* raw = (uint8_t*)msg;
+    for(uint64_t i = 0; i < size; i++) check += raw[i];
+    return (check == 0);
+}
+
+void libsigma_ipc_set_message_checksum(libsigma_message_t* msg, size_t size){
+    msg->checksum = 0;
+
+    uint8_t checksum = 0;
+    uint8_t* raw = (uint8_t*)msg;
+    for(uint64_t i = 0; i < size; i++) checksum += raw[i];
+
+    msg->checksum = 255 - checksum;
+}
+
 int libsigma_ipc_send(uint64_t dest, size_t buf_size, uint8_t* buffer){
     return libsigma_syscall3(SIGMA_SYSCALL_IPC_SEND, dest, buf_size, (uint64_t)buffer);
 }
