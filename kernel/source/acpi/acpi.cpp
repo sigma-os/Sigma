1ea9fde83b5db9da96b2a7dfd8033809319de8b9
Thomas Woertman <twoertman@gmail.com>
2019-09-27 19:26:54 +0200

    Fix ACPI table loading

--- kernel/source/acpi/acpi.cpp
+++ kernel/source/acpi/acpi.cpp
@@ -7,7 +7,7 @@ extern "C" {
 #include <lai/drivers/ec.h>
 }
 
-
+uint64_t revision = 0;
 auto acpi_tables = types::linked_list<uint64_t>();
 
 static bool do_checksum(acpi::sdt_header* header){
@@ -19,11 +19,12 @@ static bool do_checksum(acpi::sdt_header* header){
 }
 
 acpi::table* acpi::get_table(const char* signature, uint64_t index){
+    debug_printf("[ACPI]: Requesting table: %s, index: %d...", signature, index);
     if(signature == acpi::dsdt_signature){
         acpi::fadt* fadt = reinterpret_cast<acpi::fadt*>(acpi::get_table(acpi::fadt_signature));
         uint64_t dsdt_phys_addr = 0;
 
-        if(IS_CANONICAL(fadt->x_dsdt)) dsdt_phys_addr = fadt->x_dsdt;
+        if(IS_CANONICAL(fadt->x_dsdt) && revision != 0) dsdt_phys_addr = fadt->x_dsdt;
         else dsdt_phys_addr = fadt->dsdt;
 
         uint64_t dsdt_addr = (dsdt_phys_addr + KERNEL_PHYSICAL_VIRTUAL_MAPPING_BASE);
@@ -33,20 +34,24 @@ acpi::table* acpi::get_table(const char* signature, uint64_t index){
             mm::vmm::kernel_vmm::get_instance().map_page((dsdt_phys_addr + (mm::pmm::block_size * i)), (dsdt_addr + (mm::pmm::block_size * i)), map_page_flags_present | map_page_flags_cache_disable | map_page_flags_no_execute);
         }
 
+        debug_printf("Found at: %x\n", dsdt_addr);
         return reinterpret_cast<acpi::table*>(dsdt_addr);
     }
 
     uint64_t curr = 0;
-    for(auto table : acpi_tables){
-        acpi::sdt_header* header = reinterpret_cast<acpi::sdt_header*>(table);
+    for(const auto table : acpi_tables){
+        auto* header = reinterpret_cast<acpi::sdt_header*>(table);
 
         if((signature[0] == header->signature[0]) && (signature[1] == header->signature[1]) && (signature[2] == header->signature[2]) && (signature[3] == header->signature[3])){
             if(curr != index) curr++;
-            else return reinterpret_cast<acpi::table*>(header);
+            else{
+                debug_printf("Found at: %x\n", header);
+                return reinterpret_cast<acpi::table*>(header);
+            } 
         }
     }
 
-    debug_printf("[ACPI]: Couldn't find table %c%c%c%c, index: %d\n", signature[0], signature[1], signature[2], signature[3], index);
+    debug_printf("Didn't find\n");
     return nullptr;
 }
 
@@ -55,7 +60,7 @@ acpi::table* acpi::get_table(const char* signature){
         acpi::fadt* fadt = reinterpret_cast<acpi::fadt*>(acpi::get_table(acpi::fadt_signature));
         uint64_t dsdt_phys_addr = 0;
 
-        if(fadt->x_dsdt != 0) dsdt_phys_addr = fadt->x_dsdt;
+        if(IS_CANONICAL(fadt->x_dsdt) && revision != 0) dsdt_phys_addr = fadt->x_dsdt;
         else dsdt_phys_addr = fadt->dsdt;
 
         uint64_t dsdt_addr = (dsdt_phys_addr + KERNEL_PHYSICAL_VIRTUAL_MAPPING_BASE);
@@ -108,6 +113,8 @@ void acpi::init(boot::boot_protocol* boot_protocol){
         return;
     }
 
+    revision = rsdp->revision;
+
     if(rsdp->revision > 0){
         debug_printf("[ACPI]: Detected version 2 or higher\n");
         acpi::xsdp* xsdp = reinterpret_cast<acpi::xsdp*>(rsdp);
@@ -137,27 +144,20 @@ void acpi::init(boot::boot_protocol* boot_protocol){
 
         for (size_t i = 0; i < entries; i++)
         {
-
             if(reinterpret_cast<uint64_t*>(xsdt->tables[i]) == nullptr) continue;
-
-            uint64_t page_addr = xsdt->tables[i] & 0xFFFFFFFFFFFFF000;
-            mm::vmm::kernel_vmm::get_instance().map_page((page_addr + 0x1000), ((page_addr + 0x1000) + KERNEL_PHYSICAL_VIRTUAL_MAPPING_BASE), map_page_flags_present | map_page_flags_cache_disable | map_page_flags_no_execute);
             mm::vmm::kernel_vmm::get_instance().map_page(xsdt->tables[i], (xsdt->tables[i] + KERNEL_PHYSICAL_VIRTUAL_MAPPING_BASE), map_page_flags_present | map_page_flags_cache_disable | map_page_flags_no_execute);
-
             auto* h = reinterpret_cast<acpi::sdt_header*>(xsdt->tables[i] + KERNEL_PHYSICAL_VIRTUAL_MAPPING_BASE);
 
             uint64_t n_pages = common::div_ceil(h->length, mm::pmm::block_size);
-            for(uint64_t i = 0; i < n_pages; i++){
-                uint64_t phys = (xsdt->tables[i] + (i * mm::pmm::block_size));
+            for(uint64_t j = 0; j < n_pages; j++){
+                uint64_t phys = (xsdt->tables[i] + (j * mm::pmm::block_size));
                 uint64_t virt = phys + KERNEL_PHYSICAL_VIRTUAL_MAPPING_BASE;
                 mm::vmm::kernel_vmm::get_instance().map_page(phys, virt, map_page_flags_present | map_page_flags_cache_disable | map_page_flags_no_execute);
             }
 
             if(do_checksum(h)){
                 debug_printf("[ACPI]: Found Table %c%c%c%c: oem_id:%c%c%c%c%c%c, Revision: %d\n", h->signature[0], h->signature[1], h->signature[2], h->signature[3], h->oem_id[0], h->oem_id[1], h->oem_id[2], h->oem_id[3], h->oem_id[4], h->oem_id[5], h->revision);
-
                 acpi_tables.push_back(reinterpret_cast<uint64_t>(h));
-
             }
 
         }
@@ -181,10 +181,19 @@ void acpi::init(boot::boot_protocol* boot_protocol){
         for (size_t i = 0; i < entries; i++)
         {
             if(reinterpret_cast<uint64_t*>(rsdt->tables[i]) == nullptr) continue;
-            mm::vmm::kernel_vmm::get_instance().map_page(rsdt->tables[i], (rsdt->tables[i] + KERNEL_PHYSICAL_VIRTUAL_MAPPING_BASE), map_page_flags_present | map_page_flags_cache_disable | map_page_flags_no_execute);
             auto* h = reinterpret_cast<acpi::sdt_header*>(rsdt->tables[i] + KERNEL_PHYSICAL_VIRTUAL_MAPPING_BASE);
+            mm::vmm::kernel_vmm::get_instance().map_page(rsdt->tables[i], (rsdt->tables[i] + KERNEL_PHYSICAL_VIRTUAL_MAPPING_BASE), map_page_flags_present | map_page_flags_cache_disable | map_page_flags_no_execute);
+
+            uint64_t addr = rsdt->tables[i] - mm::pmm::block_size;
+            auto n_pages = common::div_ceil(h->length, mm::pmm::block_size) + 2;
+            for(uint64_t j = 0; j < n_pages; j++){
+                uint64_t phys = addr + (j * mm::pmm::block_size);
+                uint64_t virt = phys + KERNEL_PHYSICAL_VIRTUAL_MAPPING_BASE;
+                mm::vmm::kernel_vmm::get_instance().map_page(phys, virt, map_page_flags_present | map_page_flags_cache_disable | map_page_flags_no_execute);
+            }
+
             if(do_checksum(h)){
-                debug_printf("[ACPI]: Found Table %c%c%c%c: oem_id:%c%c%c%c%c%c, Revision: %d\n", h->signature[0], h->signature[1], h->signature[2], h->signature[3], h->oem_id[0], h->oem_id[1], h->oem_id[2], h->oem_id[3], h->oem_id[4], h->oem_id[5], h->revision);
+                debug_printf("[ACPI]: Found Table %c%c%c%c: oem_id:%c%c%c%c%c%c, Revision: %d, Addr: %x\n", h->signature[0], h->signature[1], h->signature[2], h->signature[3], h->oem_id[0], h->oem_id[1], h->oem_id[2], h->oem_id[3], h->oem_id[4], h->oem_id[5], h->revision, h);
 
                 acpi_tables.push_back(reinterpret_cast<uint64_t>(h));
             }
