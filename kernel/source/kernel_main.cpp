9dc2cbf24a0520ba8a5dde5963d3e1c70c90ee56
Thomas Woertman <twoertman@gmail.com>
2019-11-30 20:36:28 +0100

    kernel: Don't run ctest by default anymore
    Also remove old unused test program

--- kernel/source/kernel_main.cpp
+++ kernel/source/kernel_main.cpp
@@ -168,9 +168,6 @@ C_LINKAGE void kernel_main(){
     if(!proc::elf::start_elf_executable("/usr/bin/zeta", &thread, proc::process::thread_privilege_level::DRIVER)) printf("Failed to load Zeta\n");
     proc::syscall::set_user_manager_tid(thread->tid);
 
-    proc::process::thread* threadb = nullptr;
-    if(!proc::elf::start_elf_executable("/usr/bin/ctest", &threadb, proc::process::thread_privilege_level::APPLICATION)) printf("Failed to load ctest\n");
-
     proc::process::thread* blockdev = nullptr;
     if(!proc::elf::start_elf_executable("/usr/bin/ahci", &blockdev, proc::process::thread_privilege_level::DRIVER)) printf("Failed to load ahci\n");
 
