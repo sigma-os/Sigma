36113de930f36a873593a4e1ad7cc449e0cccad6
Thomas Woertman <twoertman@gmail.com>
2019-12-01 16:02:14 +0100

    zeta: Update log messages

--- zeta/source/main.cpp
+++ zeta/source/main.cpp
@@ -10,7 +10,7 @@
 
 template<typename Ret>
 void send_return_int([[maybe_unused]] uint64_t dest, [[maybe_unused]] uint64_t msg_id, [[maybe_unused]] Ret ret){
-    libsigma_klog("[ZETA]: Unsupported return type\n");
+    libsigma_klog("zeta: Unsupported return type\n");
     return;
 }
 
@@ -63,14 +63,14 @@ void handle_request(){
     uint64_t origin;
     size_t useless_msg_size;
     if(libsigma_ipc_receive(&origin, &useless_msg_size, msg.get()) == 1){
-        libsigma_klog("[ZETA]: Failed to receive IPC message\n");
+        libsigma_klog("zeta: Failed to receive IPC message\n");
         return;
     }
 
     auto* message = reinterpret_cast<libsigma_message_t*>(msg.get());
 
     if(!libsigma_ipc_check_message(message, msg_size)) {
-        libsigma_klog("[ZETA]: Checksum failed\n");
+        libsigma_klog("zeta: Checksum failed\n");
         return;
     }
 
@@ -145,15 +145,15 @@ void handle_request(){
 
 
 int main(){
-    libsigma_klog("[ZETA]: Starting VFS\n");
-    
     fs::devfs devfs{};
     devfs.init();
     devfs.add_file("/sysout", fs::devfs::sysout_node_factory());
 
     auto loop = [&](){
-        while(true) handle_request();
+        while(true) 
+            handle_request();
     };
+    libsigma_klog("zeta: Started VFS\n");
     loop();
 
     while(true);
