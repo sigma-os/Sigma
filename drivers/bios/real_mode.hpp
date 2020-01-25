6e9219f9ae98b2bc20747021654c3f29e9c5192b
Thomas Woertman <twoertman@gmail.com>
2020-01-25 21:26:55 +0100

    bios: Experimental VBE driver using Real Mode emulation

---
+++ drivers/bios/real_mode.hpp
@@ -0,0 +1,132 @@
+#pragma once
+
+#include <sys/mman.h>
+#include <libsigma/memory.h>
+#include <libsigma/virt.h>
+#include <stdint.h>
+#include <stdio.h>
+
+struct rm_regs {
+    uint32_t eflags;
+    uint32_t ebp;
+    uint32_t edi;
+    uint32_t esi;
+    uint32_t edx;
+    uint32_t ecx;
+    uint32_t ebx;
+    uint32_t eax;
+};
+
+struct rm_cpu {
+    public:
+    rm_cpu(){
+        vspace = vctl(vCtlCreateVspace, 0, 0, 0, 0);
+
+        vctl(vCtlMapVspacePhys, vspace, 0x0, 0x0, 0xFFFFFFFF); // Map 4GiB of ram into the VM
+
+        auto* ram = libsigma_vm_map(0x8000, NULL, NULL, PROT_READ | PROT_WRITE, MAP_ANON);
+
+        payload = ((uint8_t*)ram + 0x2C00);
+
+        data_section_addr = 0x7d00;
+        data_section = ((uint8_t*)ram + 0x2D00);
+
+        vctl(vCtlMapVspace, vspace, 0x5000, (uint64_t)ram, 0x8000);
+
+        vcpu = vctl(vCtlCreateVcpu, 0, 0, 0, 0);
+    }
+
+    void intn(int n, rm_regs& regs){
+        /*
+            int n
+            vmmcall
+        */
+
+        payload[0] = 0xCD;
+        payload[1] = n;
+
+        payload[2] = 0x0F;
+        payload[3] = 0x01;
+        payload[4] = 0xd9;
+
+        vregs cpu_regs{};
+
+        vctl(vCtlGetRegs, vcpu, (uint64_t)&cpu_regs, 0, 0);
+
+        cpu_regs.rax = regs.eax;
+        cpu_regs.rbx = regs.ebx;
+        cpu_regs.rcx = regs.ecx;
+        cpu_regs.rdx = regs.edx;
+        cpu_regs.rsi = regs.esi;
+        cpu_regs.rdi = regs.edi;
+
+        cpu_regs.rflags = regs.eflags;
+
+        cpu_regs.ds.limit = 0xFFFFFFFF;
+        cpu_regs.es.limit = 0xFFFFFFFF;
+        cpu_regs.fs.limit = 0xFFFFFFFF;
+        cpu_regs.gs.limit = 0xFFFFFFFF;
+        cpu_regs.ss.limit = 0xFFFFFFFF;
+
+        cpu_regs.cs.selector = 0;
+        cpu_regs.cs.base = 0;
+        cpu_regs.rip = 0x7C00;
+
+        cpu_regs.rsp = 0x7C00;
+
+        vctl(vCtlSetRegs, vcpu, (uint64_t)&cpu_regs, 0, 0);
+
+        // Setup state, now run
+
+        vexit exit = {};
+        while(1){
+            vctl(vCtlRunVcpu, vcpu, (uint64_t)&exit, 0, 0);
+
+            /*printf("Exit reason: %lx\nOpcode: ", exit.reason);
+        
+            for(int i = 0; i < exit.opcode_length; i++)
+                printf("0x%02X ", exit.opcode[i]);
+
+            printf("\n");*/
+
+            if(exit.reason == vCtlExitReasonInterrupt){
+                printf("Interrupt: vector: %02x\n", exit.interrupt_number);
+                break;
+            } else if(exit.reason == vCtlExitReasonPortRead){
+                switch(exit.opcode[0]){
+                    default:
+                        printf("Unknown PIO IN opcode: %x\n", exit.opcode[0]);
+                        while(1)
+                            asm("pause");
+                }
+            } else if(exit.reason == vCtlExitReasonPortWrite){
+                switch(exit.opcode[0]){
+                    default:
+                        printf("Unknown PIO OUT opcode: %x\n", exit.opcode[0]);
+                        while(1)
+                            asm("pause");
+                }
+            } else if(exit.reason == vCtlExitReasonHypercall)
+                break;
+        }
+
+        vctl(vCtlGetRegs, vcpu, (uint64_t)&cpu_regs, 0, 0);
+
+        regs.eax = cpu_regs.rax;
+        regs.ebx = cpu_regs.rbx;
+        regs.ecx = cpu_regs.rcx;
+        regs.edx = cpu_regs.rdx;
+        regs.esi = cpu_regs.rsi;
+        regs.edi = cpu_regs.rdi;
+
+        regs.eflags = cpu_regs.rflags;
+    }
+    uint8_t* data_section = nullptr;
+    uint32_t data_section_addr = 0;
+
+    private:
+
+    uint64_t vspace, vcpu;
+
+    uint8_t* payload;
+};
---
+++ drivers/bios/vbe.cpp
@@ -0,0 +1,118 @@
+#include "vbe.hpp"
+#include <string>
+#include <vector>
+#include <utility>
+#include <cstring>
+
+rm_cpu* cpu;
+std::vector<std::pair<uint16_t, vbe::vbe_mode_info_block>> supported_modes{};
+bool initialized;
+
+bool vbe::init(rm_cpu& vcpu){
+    cpu = &vcpu;
+
+    vbe_info_block* info = (vbe_info_block*)cpu->data_section;
+    strncpy(info->sig, "VBE2", 4);
+
+    rm_regs regs{};
+    regs.eax = 0x4f00;
+    regs.edi = cpu->data_section_addr;
+
+    cpu->intn(0x10, regs);
+
+    if(regs.eax != 0x4F || regs.eflags & (1 << 0)){
+        printf("bios: VBE unsupported\n");
+        return false;
+    }
+        
+    printf("bios: Detected VBE Support\n\tSignature: %.4s\n\tRevision: %d.%d\n\tTotal Memory: %dKB\n", info->sig, info->major_ver, info->minor_ver, info->total_mem * 64);
+
+    auto print_bios_string = [](const char* prefix, vbe_far_ptr ptr){
+        // Real mode segment:offset pair
+        uintptr_t str_phys = ptr.phys();
+        uintptr_t str_phys_offset = str_phys & 0xFFF;
+
+        auto* str = (const char*)libsigma_vm_map(0x1000, NULL, (void*)str_phys, PROT_READ, MAP_ANON) + str_phys_offset;
+        printf("%s%s\n", prefix, str);
+    };
+
+    print_bios_string("\tOEM String: ", info->oem);
+    print_bios_string("\tVendor String: ", info->vendor_string);
+    print_bios_string("\tProduct Name String: ", info->product_name);
+    print_bios_string("\tProduct Revision String: ", info->product_revision);
+
+    uintptr_t modes_phys = info->mode_info.phys();
+    uintptr_t modes_offset = modes_phys & 0xFFF;
+    
+    uint16_t* modes = nullptr;
+
+    if(modes_phys >= cpu->data_section_addr && modes_phys <= cpu->data_section_addr + sizeof(vbe_info_block))
+        modes = (uint16_t*)(cpu->data_section + (modes_phys - cpu->data_section_addr));
+    else
+        modes = (uint16_t*)((uintptr_t)libsigma_vm_map(0x1000, NULL, (void*)modes_phys, PROT_READ, MAP_ANON) + modes_offset);
+        
+    for(size_t i = 0; modes[i] != 0xFFFF; i++)
+        supported_modes.push_back({modes[i], {}});
+
+    for(auto& [mode, mode_info] : supported_modes){
+        vbe_mode_info_block* mode_info_block = (vbe_mode_info_block*)cpu->data_section;
+
+        regs.eax = 0x4f01;
+        regs.ecx = mode;
+        regs.edi = cpu->data_section_addr;
+
+        cpu->intn(0x10, regs);
+        if(regs.eax != 0x4F || regs.eflags & (1 << 0)){
+            printf("bios: Failed to find mode info for VBE mode 0x%04x\n", mode);
+            continue;
+        }
+
+        mode_info = *mode_info_block;
+    }
+
+    printf("\tSupported Video Modes: \n");
+    for(auto& [mode, mode_info] : supported_modes){
+        printf("\t\t0x%04x: %dx%d\n", mode, mode_info.width, mode_info.height);
+    }
+
+    initialized = true;
+
+    return true;
+}
+
+uint16_t vbe::set_mode(uint16_t width, uint16_t height, uint8_t bpp){
+    if(!initialized)
+        return -1;
+
+    uint16_t mode_to_set = 0;
+    for(auto& [mode, mode_info] : supported_modes){
+        if(mode_info.width == width && mode_info.height == height && mode_info.bpp == bpp){
+            mode_to_set = mode;
+        }
+    }
+    
+    if(mode_to_set == 0){
+        printf("bios: Couldn't find matching mode for %dx%d %d bpp", width, height, bpp);
+        return -1;
+    }
+
+    rm_regs regs{};
+    regs.eax = 0x4f02;  
+    regs.ebx = mode_to_set | (1 << 14); // Mode 0x0104, LFB, Clear screen
+
+    cpu->intn(0x10, regs);
+    if(regs.eax != 0x4F || regs.eflags & (1 << 0))
+        printf("bios: Failed to set video mode 0x%04x\n", mode_to_set);
+
+    return mode_to_set;
+}
+
+vbe::vbe_mode_info_block* vbe::get_mode_info(uint16_t mode){
+    for(auto& [search_mode, mode_info] : supported_modes){
+        if(search_mode == mode){
+            return &mode_info;
+        }
+    }
+
+    return nullptr;
+}
