9df52384896c0ba9ecdfaa0b4be6c24702eee29c
Thomas Woertman <twoertman@gmail.com>
2020-03-07 23:39:05 +0100

    kernel/misc: Make panic noreturn

--- kernel/include/Sigma/misc/panic.h
+++ kernel/include/Sigma/misc/panic.h
@@ -2,9 +2,13 @@
 #define SIGMA_KERNEL_PANIC
 
 #include <klibcxx/experimental/source_location.hpp>
+#include <Sigma/misc/compiler.h>
 
 namespace misc::panic {
+    NORETURN_ATTRIBUTE
     void panic_m(const char* message, const char* function_override, std::experimental::source_location loc);
+
+    NORETURN_ATTRIBUTE
     void panic_m(const char* message, std::experimental::source_location loc);
 }
 
--- kernel/source/misc/panic.cpp
+++ kernel/source/misc/panic.cpp
@@ -3,12 +3,14 @@
 #include <klibc/stdio.h>
 #include <klibc/stdlib.h>
 
+NORETURN_ATTRIBUTE
 void misc::panic::panic_m(const char* message, std::experimental::source_location loc){
     printf("KERNEL PANIC in file: %s in function %s on line: %d: %s\n", loc.file_name(), loc.function_name(), loc.line(), message);
     debug::trace_stack();
     abort();
 }
 
+NORETURN_ATTRIBUTE
 void misc::panic::panic_m(const char* message, const char* function_override, std::experimental::source_location loc){
     printf("KERNEL PANIC in file: %s in function %s on line: %d: %s\n", loc.file_name(), function_override, loc.line(), message);
     debug::trace_stack();
